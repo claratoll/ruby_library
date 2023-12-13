d7493a54eeec73b4975f1a88564920029e3cd658
claratoll <clara.toll@gmail.com>
2023-12-13 15:02:45 +0100

    done

--- app/controllers/user_libraries_controller.rb
+++ app/controllers/user_libraries_controller.rb
@@ -15,6 +15,19 @@ class UserLibrariesController < ApplicationController
     end
   end
 
+
+  def update
+    @user_library = current_user.user_libraries.find_by(id: params[:id])
+
+    if @user_library && @user_library.update(user_library_params)
+      redirect_to user_libraries_path, notice: 'Bokens information har uppdaterats i ditt bibliotek!'
+    else
+      redirect_to user_libraries_path, notice: 'Något gick fel. Försök igen.'
+    end
+  end
+
+  private
+
   def user_library_params
     params.require(:user_library).permit(:book_id, :status, :rating)
   end
